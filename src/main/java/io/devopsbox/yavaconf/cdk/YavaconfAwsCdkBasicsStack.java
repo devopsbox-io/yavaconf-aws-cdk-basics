fdf5d4471e553614f285f3363d518e7165aa5726
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2021-06-21 17:36:25 +0200

    Basic CDK examples

--- src/main/java/io/devopsbox/yavaconf/cdk/YavaconfAwsCdkBasicsStack.java
+++ src/main/java/io/devopsbox/yavaconf/cdk/YavaconfAwsCdkBasicsStack.java
@@ -1,8 +1,17 @@
 package io.devopsbox.yavaconf.cdk;
 
+import software.amazon.awscdk.core.CfnTag;
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.Tags;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketProps;
+import software.amazon.awscdk.services.s3.CfnBucket;
+import software.amazon.awscdk.services.s3.CfnBucketProps;
+
+import java.util.Arrays;
 
 public class YavaconfAwsCdkBasicsStack extends Stack {
     public YavaconfAwsCdkBasicsStack(final Construct scope, final String id) {
@@ -12,6 +21,29 @@ public class YavaconfAwsCdkBasicsStack extends Stack {
     public YavaconfAwsCdkBasicsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        s3BucketUsingL1Construct();
+
+        s3BucketUsingL2Construct();
+    }
+
+    private void s3BucketUsingL1Construct() {
+        new CfnBucket(this, "yavaconf-l1", CfnBucketProps.builder()
+                .bucketName("yavaconf-cdk-l1")
+                .tags(Arrays.asList(
+                        CfnTag.builder()
+                                .key("Environment")
+                                .value("prod")
+                                .build()
+                ))
+                .build());
+    }
+
+    private void s3BucketUsingL2Construct() {
+        Bucket l2Bucket = new Bucket(this, "yavaconf-l2", BucketProps.builder()
+                .bucketName("yavaconf-cdk-l2")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build());
+
+        Tags.of(l2Bucket).add("Environment", "prod");
     }
 }
